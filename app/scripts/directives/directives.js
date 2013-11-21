6213da71a8f31f7c85647dfc3722980fdb3e9911
Richard Harris <craaan@gmail.com>
2013-11-20 22:22:25 -0700

    In case you need to customize the panel resize height, use pixel-offset

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -3,7 +3,11 @@ angular.module('YHA.directives')
         return {
             restrict: 'A',
             link: function(scope, element, attrs) {
-                element.css('height', $("#"+attrs.resizeTo).height()-108 + "px")
+                var offset = 108;
+                if (attrs.pixelOffset) {
+                    offset = parseInt(attrs.pixelOffset, 10);
+                }
+                element.css('height', $("#"+attrs.resizeTo).height()-offset + "px")
             }
         };
     });
